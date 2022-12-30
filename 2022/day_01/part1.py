211b843bf61c0847b035711bf950eab0d4cd32e7
Bob Kimble <robert.kimble@uspto.gov>
2022-12-30 11:13:00 -0500

    Fixed typo in module docstrings

--- 2022/day_01/part1.py
+++ 2022/day_01/part1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Solves part 1 of the day 1 puzzle or the 2022 Advent of Code challenge."""
+"""Solves part 1 of the day 1 puzzle of the 2022 Advent of Code challenge."""
 
 from argparse import ArgumentParser
 
--- 2022/day_01/part2.py
+++ 2022/day_01/part2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Solves part 2 of the day 1 puzzle or the 2022 Advent of Code challenge."""
+"""Solves part 2 of the day 1 puzzle of the 2022 Advent of Code challenge."""
 
 
 from part1 import get_args
